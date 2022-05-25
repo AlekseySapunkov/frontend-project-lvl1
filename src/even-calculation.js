38edf722d71634b9ff1eab53a6d80447a5e0c71c
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-05-25 15:45:51 +0300

    Update even-calculation.js

--- src/even-calculation.js
+++ src/even-calculation.js
@@ -1,11 +1,12 @@
 import readlineSync from 'readline-sync';
 
-import greeting from '../src/cli.js';
-
 import getRandomInt from '../src/random.js';
 
 export default function evenCalculate() {
-  greeting();
+    console.log('Welcome to the Brain Games!');
+    const playerName = readlineSync.question('May I have your name? ');
+    const greetingOfPlayer = `Hello, ${playerName}!`;
+    console.log(greetingOfPlayer);
   console.log(`Answer "yes" if the number is even, otherwise answer "no"`);
   let counter = 0;
   while (counter < 3) {
@@ -20,8 +21,16 @@ export default function evenCalculate() {
     console.log('Correct!');
     counter +=1;
     }
+    else if (randomNumber%2 === 0 && answerOfPlayer !== 'yes') {
+        console.log (`\'${answerOfPlayer}\' is wrong answer ;( Correct answer was \'yes\'\nLet\'s try again, ${playerName}!`);
+        break;
+    }
     else {
-        console.log (`${answerOfPlayer}`)
+        console.log (`\'${answerOfPlayer}\' is wrong answer ;( Correct answer was \'no\'\nLet\'s try again, ${playerName}!`);
+        break;
     }
   }
+  if (counter >=3){
+  console.log (`Congratulations, ${playerName}!`);
+  };
 }
