acbd8d1d6fb1dbc57e138a57b7ca29a12da3cbd9
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-05-28 16:48:41 +0300

    Update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,5 +1,31 @@
+import questAnswer from '../index.js';
+import { getRandomInt } from '../random.js';
 
-const greatComDiv = () => {
-
+export default function greatComDiv() {
+  const task = 'Find the greatest common divisor of given numbers.';
+  const result = [];
+  const question = [];
+  let elementOfResult;
+  let elementOfQuestion;
+  for (let i = 0; i < 3; i += 1) {
+    let firstOperand = getRandomInt(1, 50);
+    let secondOperand = getRandomInt(1, 50);
+    elementOfQuestion = `${firstOperand} ${secondOperand}`;
+    question.push(elementOfQuestion);
+    if (firstOperand === 1 || secondOperand === 1) {
+      elementOfResult = 1;
+      result.push(elementOfResult);
+    }
+    else if (firstOperand === secondOperand) {
+      elementOfResult = firstOperand;
+      result.push(elementOfResult);
+    } else {
+      while (firstOperand && secondOperand) {
+        firstOperand > secondOperand ? firstOperand %= secondOperand : secondOperand %= firstOperand;
+        }
+        elementOfResult = firstOperand + secondOperand;
+        result.push(elementOfResult);
+    }
+  }
+  questAnswer(question, result, task);
 };
-export default greatComDiv();
