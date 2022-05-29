f508ac146d3d3a22fd7d7d53636bd510880aa771
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-05-29 15:38:35 +0300

    Update even-calculation.js

--- src/games/even-calculation.js
+++ src/games/even-calculation.js
@@ -1,33 +1,19 @@
-import readlineSync from 'readline-sync';
-
+import questAnswer from '../index.js';
 import { getRandomInt } from '../random.js';
 
 export default function evenCalculate() {
-  console.log('Welcome to the Brain Games!');
-  const playerName = readlineSync.question('May I have your name? ');
-  const greetingOfPlayer = `Hello, ${playerName}!`;
-  console.log(greetingOfPlayer);
-  console.log('Answer "yes" if the number is even, otherwise answer "no"');
-  let counter = 0;
-  while (counter < 3) {
+  const task = 'Answer "yes" if the number is even, otherwise answer "no"';
+  const result = [];
+  const question = [];
+  for (let i = 0; i < 3; i += 1) {
     const randomNumber = getRandomInt(0, 100);
-    console.log(`Question: ${randomNumber}`);
-    const answerOfPlayer = readlineSync.question('Your answer: ');
-    if (randomNumber % 2 === 0 && answerOfPlayer === 'yes') {
-      console.log('Correct!');
-      counter += 1;
-    } else if (randomNumber % 2 !== 0 && answerOfPlayer === 'no') {
-      console.log('Correct!');
-      counter += 1;
-    } else if (randomNumber % 2 === 0 && answerOfPlayer !== 'yes') {
-      console.log(`'${answerOfPlayer}' is wrong answer ;( Correct answer was 'yes'\nLet's try again, ${playerName}!`);
-      break;
-    } else {
-      console.log(`'${answerOfPlayer}' is wrong answer ;( Correct answer was 'no'\nLet's try again, ${playerName}!`);
-      break;
+    question.push(randomNumber);
+    let isPrime = true;
+    if (randomNumber % 2 !== 0) {
+      isPrime = false;
     }
+    const answer = isPrime ? 'yes' : 'no';
+    result.push(answer);
   }
-  if (counter >= 3) {
-    console.log(`Congratulations, ${playerName}!`);
-  }
+  questAnswer(question, result, task);
 }
