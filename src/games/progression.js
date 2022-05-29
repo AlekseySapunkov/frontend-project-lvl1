6ab5723d56f6ab3ebeb54fccceab1f3b4f9e256f
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-05-29 12:11:44 +0300

    Update progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,6 +1,32 @@
 import questAnswer from '../index.js';
-import { getRandomInt } from '../random.js';
+import { getRandomInt, getRandomMis } from '../random.js';
 
 export default function progression() {
-    
+  const task = 'What number is missing in the progression?';
+  const result = [];
+  const question = [];
+  let elementOfResult;
+  let elementOfQuestion = [];
+  for (let i = 0; i < 3; i += 1) {
+    const len = getRandomInt(5, 10);
+    const firstElement = getRandomInt(0, 50);
+    const progress = getRandomInt(1, 10);
+    const array = [];
+    array[0] = firstElement;
+    const arrayModified = [];
+    arrayModified[0] = firstElement;
+    for (let k = 0; k < len - 1; k += 1) {
+      array.push(array[k] + progress);
+      arrayModified.push(arrayModified[k] + progress);
+    }
+    elementOfQuestion = getRandomMis(arrayModified, len);
+    question.push(elementOfQuestion);
+    for (let j = 0; j < elementOfQuestion.length; j += 1) {
+      if (elementOfQuestion[j] === '..') {
+        elementOfResult = array[j];
+        result.push(elementOfResult);
+      }
+    }
+  }
+  questAnswer(question, result, task);
 }
