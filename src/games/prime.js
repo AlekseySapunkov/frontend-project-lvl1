5d69a7193d43726bb69d963182e46a116042b82d
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-05-29 13:34:24 +0300

    Create prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,10 @@
+import questAnswer from '../index.js';
+import { getRandomInt, getRandomMis } from '../random.js';
+
+export default function gameIsPrime() {
+    const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    const result = [];
+    const question = [];
+    let elementOfResult;
+    let elementOfQuestion = [];
+}
