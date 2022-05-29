49429c8b2f7683a68b695faaadf66bf6c11aa495
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-05-29 15:20:58 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -8,7 +8,7 @@ export default function gameIsPrime() {
   for (let i = 0; i < 3; i += 1) {
     const randomNumber = getRandomInt(2, 100);
     let isPrime = true;
-    question.push(`Question: ${randomNumber}`);
+    question.push(randomNumber);
     if (randomNumber !== 2) {
       for (let j = 2; j < Math.sqrt(randomNumber); j += 1) {
         if (randomNumber % j === 0) {
