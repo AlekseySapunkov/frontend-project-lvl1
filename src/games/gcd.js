bdc6bfb1afc1cfd49638674d00d0fb85bf7ae932
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-05-31 11:24:29 +0300

    Update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -11,14 +11,7 @@ export default function greatComDiv() {
     let firstNumber = getRandomInt(1, 50);// ранодомно выбираем первое число
     let secondNumber = getRandomInt(1, 50);
     elementOfQuestion = `${firstNumber} ${secondNumber}`;
-    question.push(elementOfQuestion);
-    if (firstNumber === 1 || secondNumber === 1) {
-      elementOfResult = 1;
-      result.push(elementOfResult);
-    } else if (firstNumber === secondNumber) {
-      elementOfResult = firstNumber;
-      result.push(elementOfResult);
-    } else {
+    question.push(elementOfQuestion);  
       while (firstNumber && secondNumber) { // нахождение наивысшего общего делителя
         if (firstNumber > secondNumber) {
           firstNumber %= secondNumber;
@@ -29,6 +22,5 @@ export default function greatComDiv() {
       elementOfResult = firstNumber + secondNumber;
       result.push(elementOfResult);
     }
-  }
   questAnswer(question, result, task);
 }
