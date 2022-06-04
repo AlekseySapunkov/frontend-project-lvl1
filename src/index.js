dff8c279976b3dcfda247101033880cb128c1642
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-06-04 16:57:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import readlineSync from 'readline-sync';
 
-export default function start(quest, answer, task) {
+export default function start(question, answer, exercise) {
   console.log('Welcome to the Brain Games!');// Приветствие
   const userName = readlineSync.question('May I have your name? ');// Спрашиваем имя игрока
   console.log(`Hello, ${userName}!`);
-  console.log(task);
-  for (let i = 0; i < quest.length; i += 1) {
-    console.log(`Question: ${quest[i]}`);// Записываем условие первой задачи
+  console.log(exercise);
+  for (let i = 0; i < question.length; i += 1) {
+    console.log(`Question: ${question[i]}`);// Записываем условие первой задачи
     const userAnswer = (readlineSync.question('Your answer: '));// Считываем ответ игрока
     if (String(answer[i]) !== userAnswer) { // проверяем правильность ответа игрока
       console.log(`'${(userAnswer)}' is wrong answer ;(. Correct answer was '${answer[i]}'.\nLet's try again, ${userName}!`);
