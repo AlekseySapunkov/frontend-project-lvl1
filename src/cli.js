bbd41342eca4c1be3f80b6ea9e6a045b9f792bf5
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-06-04 17:14:21 +0300

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default function startGame() {
   console.log('Welcome to the Brain Games!');
-  const greetingOfUser = `Hello, ${readlineSync.question('May I have your name? ')}!`;
-  console.log(greetingOfUser);
+  console.log(`Hello, ${readlineSync.question('May I have your name? ')}!`);
 }
