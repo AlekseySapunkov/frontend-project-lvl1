7d93770a2f029fb37f71cc020964034dd8eebd34
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-06-04 16:25:39 +0300

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-export default function getRandomInt(min = 0, max = 50) { 
+export default function getRandomInt(min = 0, max = 50) {
   const minimal = Math.ceil(min);
   const maximal = Math.floor(max);
   return Math.floor(Math.random() * (maximal - minimal)) + minimal;
