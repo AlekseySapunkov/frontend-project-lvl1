43e6de54cab53fd724b551386e7b078ec8e090ec
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-06-06 13:22:01 +0300

    Update calculator.js

--- src/games/calculator.js
+++ src/games/calculator.js
@@ -1,36 +1,37 @@
 import getRandomInt from '../utils.js';
-import { roundsCount, start } from '../index.js';
+import start from '../index.js';
 
-export default function playGame() {
-  const getRandomOperator = () => {
-    const arrayOfOperator = ['+', '-', '*'];
-    return arrayOfOperator[getRandomInt(0, 2)];
-  };
-  const description = 'What is the result of the expression?';
-  const rounds = [];
-  let answer;
+const getRandomOperator = () => {
+  const arrayOfOperator = ['+', '-', '*'];
+  return arrayOfOperator[getRandomInt(0, 2)];
+};
+
+const description = 'What is the result of the expression?';
+
+const game = () => {
+  const firstOperand = getRandomInt(1, 50);
+  const secondOperand = getRandomInt(1, 50);
+  const operator = getRandomOperator();
   let question;
-  for (let i = 0; i < roundsCount; i += 1) { // для задачи выбираем рандомно два числа и операцию
-    const firstOperand = getRandomInt(1, 50);
-    const secondOperand = getRandomInt(1, 50);
-    const operator = getRandomOperator();
-    switch (operator) {
-      case '+':// проверяем  какая операция и делаем соответствующие вычисления
-        answer = String(firstOperand + secondOperand);
-        question = `${firstOperand} + ${secondOperand}`;
-        break;
-      case '-':
-        answer = String(firstOperand - secondOperand);
-        question = `${firstOperand} - ${secondOperand}`;
-        break;
-      case '*':
-        answer = String(firstOperand * secondOperand);
-        question = `${firstOperand} * ${secondOperand}`;
-        break;
-      default:
-        break;
-    }
-    rounds.push([question, answer]);
+  let answer;
+  switch (operator) {
+    case '+':// проверяем  какая операция и делаем соответствующие вычисления
+      answer = String(firstOperand + secondOperand);
+      question = `${firstOperand} + ${secondOperand}`;
+      break;
+    case '-':
+      answer = String(firstOperand - secondOperand);
+      question = `${firstOperand} - ${secondOperand}`;
+      break;
+    case '*':
+      answer = String(firstOperand * secondOperand);
+      question = `${firstOperand} * ${secondOperand}`;
+      break;
+    default:
+       break;
   }
-  start(rounds, description);
+  return [question, answer];
+}
+export default function playGame() {
+  start (game, description);
 }
