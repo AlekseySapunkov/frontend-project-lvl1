63047459e25b76450f877c1681ccec2621343b21
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-06-06 13:22:08 +0300

    Update gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,24 +1,23 @@
-import { roundsCount, start } from '../index.js';
+import start from '../index.js';
 import getRandomInt from '../utils.js';
 
-export default function startGame() {
-  const description = 'Find the greatest common divisor of given numbers.';
-  const rounds = [];
-  let question;
-  let answer;
-  for (let i = 0; i < roundsCount; i += 1) {
-    let firstNumber = getRandomInt(1, 50);
-    let secondNumber = getRandomInt(1, 50);
-    question = `${firstNumber} ${secondNumber}`;
-    while (firstNumber && secondNumber) { // нахождение наивысшего общего делителя
-      if (firstNumber > secondNumber) {
-        firstNumber %= secondNumber;
-      } else {
+const description = 'Find the greatest common divisor of given numbers.';
+
+const game = () => {
+  let firstNumber = getRandomInt(1, 50);
+  let secondNumber = getRandomInt(1, 50);
+  const question = `${firstNumber} ${secondNumber}`;
+  while (firstNumber && secondNumber) { // нахождение наивысшего общего делителя
+    if (firstNumber > secondNumber) {
+      firstNumber %= secondNumber;
+    } else {
         secondNumber %= firstNumber;
       }
     }
-    answer = String(firstNumber + secondNumber);
-    rounds.push([question, answer]);
+  const answer = String(firstNumber + secondNumber);
+  return [question, answer];
   }
-  start(rounds, description);
+
+export default function startGame() {
+  start (game, description);
 }
