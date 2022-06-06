5c5d3c9a501c45aa8d9b1ca5cbaa3793d7ff57bc
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-06-06 13:22:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import readlineSync from 'readline-sync';
 
 export const roundsCount = 3;
-export function start(rounds, description) {
+export default function start(game, description) {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(description);
   for (let i = 0; i < roundsCount; i += 1) {
-    const [question, answer] = rounds[i];
+    const [question, answer] = game();
     console.log(`Question: ${question}`);
     const userAnswer = (readlineSync.question('Your answer: '));
     if (answer !== userAnswer) { // проверяем правильность ответа игрока
