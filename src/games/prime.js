57c18ba01a827b9e37d29687a06709188d6fe524
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-06-06 13:22:11 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,24 +1,24 @@
-import { roundsCount, start } from '../index.js';
+import start from '../index.js';
 import getRandomInt from '../utils.js';
 
-export default function startGame() {
-  const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const rounds = [];
-  let question;
-  let answer;
-  for (let i = 0; i < roundsCount; i += 1) {
-    question = getRandomInt(2, 100);
-    let isPrime = true;
-    if (question !== 2) { // логика вычисления простого числа
-      for (let j = 2; j < question; j += 1) {
-        if (question % j === 0) {
-          isPrime = false;
-          break;
-        }
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const game = () => {
+  const question = getRandomInt(2, 100);
+  let isPrime = true;
+  if (question !== 2) { // логика вычисления простого числа
+    for (let j = 2; j < question; j += 1) {
+      if (question % j === 0) {
+        isPrime = false;
+        break;
       }
-      answer = isPrime ? 'yes' : 'no';// выбираем что записать в ответ
-      rounds.push([question, answer]);
     }
+  const answer = isPrime ? 'yes' : 'no';// выбираем что записать в ответ
+  return [question, answer];
   }
-  start(rounds, description);
+};
+
+export default function startGame() {
+  start (game, description);
 }
