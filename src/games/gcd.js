a14ba011dfd4ad8bfb8a2325357d31fdedc48c2e
AlekseySapunkov <88789118+AlekseySapunkov@users.noreply.github.com>
2022-06-07 14:33:51 +0300

    Add function gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,18 +3,22 @@ import getRandomInt from '../utils.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
+const gcd = (number1, number2) => {
+  while (number1 && number2) { // нахождение наивысшего общего делителя
+    if (number1 > number2) {
+      number1 %= number2;
+    } else {
+      number2 %= number1;
+    }
+  }
+  return number1 + number2;
+};
+
 const game = () => {
   let firstNumber = getRandomInt(1, 50);
   let secondNumber = getRandomInt(1, 50);
   const question = `${firstNumber} ${secondNumber}`;
-  while (firstNumber && secondNumber) { // нахождение наивысшего общего делителя
-    if (firstNumber > secondNumber) {
-      firstNumber %= secondNumber;
-    } else {
-      secondNumber %= firstNumber;
-    }
-  }
-  const answer = String(firstNumber + secondNumber);
+  const answer = String(gcd(firstNumber, secondNumber));
   return [question, answer];
 };
 
